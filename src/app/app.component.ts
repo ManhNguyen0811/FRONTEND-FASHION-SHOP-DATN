61d2c4d4e099af2c2e330c83bc58e8f0887b9641
ManhNguyen0811 <manhnguyen0916697145@gmail.com>
2024-12-16 16:59:55 +0700

    commit dau tien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./Component/Header/header/header.component";
+import { FooterComponent } from "./Component/Footer/footer/footer.component";
+import { HomeComponent } from './Component/Home/home/home.component';
+import { ProductComponent } from './Component/Product/product/product.component';
+import {isPlatformBrowser, NgIf} from '@angular/common';
+ 
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/Home/home/home.component';
+import { ProductComponent } from './Component/Product/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'product', component: ProductComponent },
+
+];
