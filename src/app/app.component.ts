99da52c646daae9aa1b8603f7c285c803d68d43c
Nguyên Phát <139315982+Phatcr812@users.noreply.github.com>
2024-12-17 10:53:32 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./Component/Header/header/header.component";
-import { FooterComponent } from "./Component/Footer/footer/footer.component";
-import { HomeComponent } from './Component/Home/home/home.component';
-import { ProductComponent } from './Component/Product/product/product.component';
+import { HeaderComponent } from "./Component/header/header.component";
+import { FooterComponent } from "./Component/footer/footer.component";
+import { HomeComponent } from './Component/home/home.component';
+import { ProductComponent } from './Component/product/product.component';
 import {isPlatformBrowser, NgIf} from '@angular/common';
  
 
