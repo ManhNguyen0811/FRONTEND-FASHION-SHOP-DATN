482aeea3677bea42e7de8900b5d6c6ecfe3af3df
Phatcr812 <chauphat176@gmail.com>
2024-12-17 15:56:54 +0700

    add header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './Component/Home/home/home.component';
-import { ProductComponent } from './Component/Product/product/product.component';
+import { HomeComponent } from './Component/home/home.component';
+import { ProductComponent } from './Component/product/product.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
