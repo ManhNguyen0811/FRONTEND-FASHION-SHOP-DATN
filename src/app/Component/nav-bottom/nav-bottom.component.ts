dd5f12494955f68748b99a935983f8d59be963b0
Phatcr812 <chauphat176@gmail.com>
2024-12-18 11:55:33 +0700

    add category to nav-bottom

--- src/app/Component/nav-bottom/nav-bottom.component.ts
+++ src/app/Component/nav-bottom/nav-bottom.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NgClass} from '@angular/common';
 import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-nav-bottom',
   standalone: true,
   imports: [
     NgClass,
-    CommonModule
+    CommonModule,
+    HeaderComponent
   ],
   templateUrl: './nav-bottom.component.html',
   styleUrl: './nav-bottom.component.scss'
@@ -21,10 +23,10 @@ export class NavBottomComponent {
   }
 
   categories = [
-    { name: 'ĐỒ MẶC NGOÀI', img: 'assets/outerwear.png' },
-    { name: 'QUẦN', img: 'assets/pants.png' },
-    { name: 'ĐỒ MẶC NHÀ', img: 'assets/homewear.png' },
-    { name: 'PHỤ KIỆN', img: 'assets/accessories.png' },
+    { name: 'ĐỒ MẶC NGOÀI', img: 'assets/images/thoitrangnu.jpg' },
+    { name: 'QUẦN', img: 'assets/images/thoitrangnu.jpg' },
+    { name: 'ĐỒ MẶC NHÀ', img: 'assets/images/thoitrangnu.jpg' },
+    { name: 'PHỤ KIỆN', img: 'assets/images/hinhDoc1.webp' },
     { name: 'HEATTECH', img: 'assets/heattech.png' },
     { name: 'UT: Áo Thun', img: 'assets/ut.png' },
     { name: 'AIRism', img: 'assets/airism.png' },
