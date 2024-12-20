7a10cc9d400b72bdb3343acc9b389eb260614fb1
ManhNguyen0811 <manhnguyen0916697145@gmail.com>
2024-12-20 16:28:33 +0700

    cart page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,16 @@ import { HomeComponent } from './Component/home/home.component';
 import { ProductComponent } from './Component/product/product.component';
 import { NavBottomComponent} from './Component/nav-bottom/nav-bottom.component';
 import {isPlatformBrowser, NgIf} from '@angular/common';
+import { CartComponent } from './Component/cart/cart.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,ProductComponent, NavBottomComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,ProductComponent, NavBottomComponent,
+    CartComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Component/home/home.component';
 import { ProductComponent } from './Component/product/product.component';
+import { CartComponent } from './Component/cart/cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'product', component: ProductComponent },
+    { path: 'cart', component: CartComponent },
 
 ];
