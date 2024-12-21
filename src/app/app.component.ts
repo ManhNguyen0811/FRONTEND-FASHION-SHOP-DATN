9fd574207010ef24813a92ef93de79f8360d3c8a
Tai <bvt.vantai@gmail.com>
2024-12-21 20:52:53 +0700

    add page checkout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,25 @@ import { HomeComponent } from './Component/home/home.component';
 import { ProductComponent } from './Component/product/product.component';
 import { NavBottomComponent} from './Component/nav-bottom/nav-bottom.component';
 import {isPlatformBrowser, NgIf} from '@angular/common';
+import {CheckoutPaymentComponent} from './Component/checkout-payment/checkout-payment.component';
+import {CheckoutDeliveryComponent} from './Component/checkout-delivery/checkout-delivery.component';
+import {CouponComponent} from './Component/coupon/coupon.component';
+import {OrderSummaryComponent} from './Component/order-summary/order-summary.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,ProductComponent, NavBottomComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,HomeComponent,ProductComponent, NavBottomComponent,
+    CheckoutPaymentComponent,CheckoutDeliveryComponent,CouponComponent,OrderSummaryComponent
+
+
+
+
+
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Component/home/home.component';
 import { ProductComponent } from './Component/product/product.component';
+import {CheckoutDeliveryComponent} from './Component/checkout-delivery/checkout-delivery.component';
+import {CheckoutPaymentComponent} from './Component/checkout-payment/checkout-payment.component';
+import {CouponComponent} from './Component/coupon/coupon.component';
+import {OrderSummaryComponent} from './Component/order-summary/order-summary.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'product', component: ProductComponent },
+    { path: 'checkout-delivery', component: CheckoutDeliveryComponent },
+    { path: 'checkout-payment', component: CheckoutPaymentComponent },
+    { path: 'coupon', component: CouponComponent },
+  { path: 'order-summary', component: OrderSummaryComponent },
 
 ];
