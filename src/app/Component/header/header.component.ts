f7c2feb444b9ddc5f54e8a664999bf4e646a2275
Phatcr812 <chauphat176@gmail.com>
2024-12-22 16:38:19 +0700

    save 1

--- src/app/Component/header/header.component.ts
+++ src/app/Component/header/header.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { Router } from '@angular/router';
+import {NgClass} from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isHome: boolean = false;
 
+  constructor(private router: Router) {
+    // Kiểm tra nếu đường dẫn hiện tại là "/"
+    this.router.events.subscribe(() => {
+      this.isHome = this.router.url === '/';
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Component/home/home.component';
 import { ProductComponent } from './Component/product/product.component';
+import {ProductDetailComponent} from './Component/product-detail/product-detail.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'product', component: ProductComponent },
-
+    { path: 'product-detail', component: ProductDetailComponent },
 ];
