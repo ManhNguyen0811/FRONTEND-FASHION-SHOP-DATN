89dea07f9fa155987e0f6f6864169a94f2597aa8
ManhNguyen0811 <manhnguyen0916697145@gmail.com>
2024-12-24 17:11:01 +0700

    Save changes before switching branches

--- src/app/Component/profile/profile.component.ts
+++ src/app/Component/profile/profile.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { OrderHistoryComponent } from './order-history/order-history.component';
-import {   RouterLink, RouterOutlet } from '@angular/router';
+import {   NavigationEnd, NavigationStart, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [OrderHistoryComponent,RouterOutlet
+  imports: [OrderHistoryComponent,RouterOutlet, CommonModule
     ,RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  isLoading = false;
 
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+        this.cdr.detectChanges();
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.isLoading = false;
+          this.cdr.detectChanges();
+        }, 200); 
+      }
+    });
+  }
+  
 }
